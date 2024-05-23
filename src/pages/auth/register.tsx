e63a97f6d35fd5473eba2f9f184fae41b20178de
Zein Hasyim <zeinhasyim111@gmail.com>
2024-05-23 11:07:40 +0700

    feat: register

---
+++ src/pages/auth/register.tsx
@@ -0,0 +1,11 @@
+import RegisterView from "@/components/auth/Register"
+
+const RegisterPage = () => {
+    return (
+        <>
+        <RegisterView/>
+        </>
+    )
+}
+
+export default RegisterPage;
