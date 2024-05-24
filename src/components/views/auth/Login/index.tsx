bc7cd607743b616bdcdfc3f4db3a593e0993cd92
Zein Hasyim <zeinhasyim111@gmail.com>
2024-05-24 13:17:38 +0700

    feat:login with google

--- src/components/views/auth/Login/index.tsx
+++ src/components/views/auth/Login/index.tsx
@@ -4,38 +4,38 @@ import Link from "next/link";
 import { FormEvent, useState } from "react";
 import { signIn } from "next-auth/react";
 
+
 const LoginView = () => {
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
 
   const { push, query } = useRouter();
 
-  const callbackUrl: any = query.callbackUrl || '/';
+  const callbackUrl: any = query.callbackUrl || "/";
   const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     setIsLoading(true);
-    setError('');
+    setError("");
     const form = event.target as HTMLFormElement;
     try {
-        const res = await signIn('credentials', {
-            redirect: false,
-            email: form.email.value,
-            password: form.password.value,
-            callbackUrl,
-        });
+      const res = await signIn("credentials", {
+        redirect: false,
+        email: form.email.value,
+        password: form.password.value,
+        callbackUrl,
+      });
 
-        if(!res?.error){
-            setIsLoading(false)
-            form.reset();
-            push(callbackUrl);
-        } else {
-            setIsLoading(false);
-            setError('Email or password is incorrect');
-        };          
-        
-    } catch(error) {
+      if (!res?.error) {
         setIsLoading(false);
-        setError('Email or password is incorrect');
+        form.reset();
+        push(callbackUrl);
+      } else {
+        setIsLoading(false);
+        setError("Email or password is incorrect");
+      }
+    } catch (error) {
+      setIsLoading(false);
+      setError("Email or password is incorrect");
     }
   };
 
@@ -52,7 +52,7 @@ const LoginView = () => {
               id="email"
               type="email"
               className={styles.login__form__item__input}
-            />  
+            />
           </div>
           <div className={styles.login__form__item}>
             <label htmlFor="password">Password</label>
@@ -63,11 +63,25 @@ const LoginView = () => {
               className={styles.login__form__item__input}
             />
           </div>
-          <button type="submit" className={styles.login__form__button}>{isLoading ? 'Loading...' : 'Login'}</button>
+          <button type="submit" className={styles.login__form__button}>
+            {isLoading ? "Loading..." : "Login"}
+          </button>
         </form>
+        <hr className={styles.login__form__divider} />
+        <div className={styles.login__form__other}>
+          <button
+            type="button"
+            onClick={() => signIn("google", { callbackUrl, redirect: false })}
+            className={styles.login__form__other__button}
+          >
+            <i className="bx bxl-google"/>
+            Login with Google
+          </button>
+        </div>
       </div>
       <p className={styles.login__link}>
-        Don{"'"}t have an account? Sign up <Link href="/auth/register">here</Link>
+        Don{"'"}t have an account? Sign up{" "}
+        <Link href="/auth/register">here</Link>
       </p>
     </div>
   );
