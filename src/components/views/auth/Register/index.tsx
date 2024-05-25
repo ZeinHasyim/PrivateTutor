89e6f9a46b8feb2b98d7d691c17a4d0919620052
Zein Hasyim <zeinhasyim111@gmail.com>
2024-05-25 10:57:59 +0700

    feat: instance fetch axios

--- src/components/views/auth/Register/index.tsx
+++ src/components/views/auth/Register/index.tsx
@@ -1,9 +1,10 @@
 import { useRouter } from "next/router";
 import styles from "./Register.module.scss";
-import Link from "next/link";
 import { FormEvent, useState } from "react";
 import Input from "@/components/ui/input";
 import Button from "@/components/ui/input/Button";
+import authServices from "@/services/auth";
+import AuthLayout from "@/components/layouts/AuthLayout";
 const RegisterView = () => {
   const [isLoading, setisLoading] = useState(false);
   const [error, setError] = useState("");
@@ -21,13 +22,7 @@ const RegisterView = () => {
       password: form.password.value,
     };
 
-    const result = await fetch("/api/user/register", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
+    const result = await authServices.registerAccount(data);
 
     if (result.status === 200) {
       form.reset();
@@ -40,24 +35,26 @@ const RegisterView = () => {
   };
 
   return (
-    <div className={styles.register}>
-      <h1 className={styles.register__title}>Register</h1>
-      {error && <p className={styles.register__error}>{error}</p>}
-      <div className={styles.register__form}>
-        <form onSubmit={handleSubmit}>
-          <Input label="Email" name="email" type="email" />
-          <Input label="Fullname" name="fullname" type="text" />
-          <Input label="Phone" name="phone" type="number" />
-          <Input label="Password" name="password" type="password" />
-          <Button type="submit" className={styles.register__form__button} variant="primary">
-            {isLoading ? "Loading..." : "Register"}
-          </Button>
-        </form>
-      </div>
-      <p className={styles.register__link}>
-        Have an account? Sign in <Link href="/auth/login">here</Link>
-      </p>
-    </div>
+    <AuthLayout
+      title="Register"
+      error={error}
+      link="/auth/login"
+      linkText="Have an account? Sign in "
+    >
+      <form onSubmit={handleSubmit}>
+        <Input label="Email" name="email" type="email" />
+        <Input label="Fullname" name="fullname" type="text" />
+        <Input label="Phone" name="phone" type="number" />
+        <Input label="Password" name="password" type="password" />
+        <Button
+          type="submit"
+          className={styles.register__button}
+          variant="primary"
+        >
+          {isLoading ? "Loading..." : "Register"}
+        </Button>
+      </form>
+    </AuthLayout>
   );
 };
 
